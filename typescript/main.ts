694ee155efbcd5f9e5c3ab4f9ae45740caa63605
CalvinSithideth <calvin.sithideth@gmail.com>
2019-04-27 20:07:09 -0700

    Added comments; changed tsc target

--- typescript/main.ts
+++ typescript/main.ts
@@ -14,6 +14,8 @@ class Student {
 }
 
 // test code
+// Create a new instance, creating an object,
+// instantiating an object
 let testStu:Student = new Student();
 testStu.firstName = "Joe";
 testStu.lastName = "Ortiz";
@@ -30,6 +32,10 @@ function registerStudent() {
     displayStudent(nextStudent);
 }
 
+/**
+ * Gets user input from the form and
+ * wraps it in a student object
+ */
 function getStudent():Student {
     let s = new Student();
     s.firstName = (<HTMLInputElement> document.getElementById("first-name")).value;
